ea477ff094821eb28c5debbb70148dfcd0ecf8f7
hykiaj <julia.emmanoele.cruz@escola.pr.gov.br>
2025-10-20 12:09:57 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Animação do banner com efeito de zoom + fade-in
 window.addEventListener("load", () => {
   const banner = document.querySelector(".banner1 img");
-
   banner.style.opacity = "0";
   banner.style.transform = "scale(1.1)";
   banner.style.transition = "all 1.5s ease";
-
   setTimeout(() => {
     banner.style.opacity = "1";
     banner.style.transform = "scale(1)";
